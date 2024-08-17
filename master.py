af9d5591f69ed38908c71ce9b10c25e30ac24138
tanghuluei <148530599+tanghuluei@users.noreply.github.com>
2024-08-18 02:57:56 +0800

    master.py

--- master.py
+++ master.py
@@ -207,7 +207,7 @@ class Master(object):
             log.warning('ssl module is NOT valid in this machine! Fallback to plain')
             return None
 
-        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)  #我更正的,通过SSLContext来创建ssl上下文
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)  #我添加的,通过SSLContext来创建ssl上下文,原本是通过create_default_context来创建ssl上下文的
         #ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
         ctx.check_hostname = False
         #ctx.load_default_certs(ssl.Purpose.SERVER_AUTH)
